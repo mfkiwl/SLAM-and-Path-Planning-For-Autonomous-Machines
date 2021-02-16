64b5a01ed90c0c87640037b343454abb3d5df0ec
Aditya <adityang5@gmail.com>
2021-02-16 11:47:06 +0530

    Run script

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@ cd ~/slam_path_planning
 
 # 16.04.7 LTS (Xenial Xerus)
 
-if  grep '^VERSION="16.04.7 LTS (Xenial Xerfazswfaerus)"$' /etc/os-release ; then
+if grep '^VERSION="16.04.7 LTS (Xenial Xerus)"$' /etc/os-release ; then
 	echo "Updating..."
 	exit
 #git fetch --all
