4a9eddc26fc5fa758e17a24938e84a6071f41ff1
Aditya <adityang5@gmail.com>
2021-02-16 11:45:36 +0530

    Run script

--- run.sh
+++ run.sh
@@ -2,12 +2,12 @@ cd ~/slam_path_planning
 
 # 16.04.7 LTS (Xenial Xerus)
 
-if [[grep '^VERSION="16.04.7 LTS (Xenial Xerfazswfaerus)"$' /etc/os-release]]; then
+if  grep '^VERSION="16.04.7 LTS (Xenial Xerfazswfaerus)"$' /etc/os-release ; then
 	echo "Updating..."
 	exit
-	git fetch --all
-	git reset --hard origin/main
-	git pull
+#git fetch --all
+#git reset --hard origin/main
+#git pull
 else
 
 	echo "Not inside ROS Container; Halting"
